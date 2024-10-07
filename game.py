37d49ae2fc9cae345e04db1d4f6ba740ea8e1b96
naakjgf <naakjgf@gmail.com>
2024-10-07 18:04:21 -0400

    updated briefly

--- game.py
+++ game.py
@@ -1,23 +1,20 @@
-import time
-import flappy_bird_gym
-env = flappy_bird_gym.make("FlappyBird-v0")
+import flappy_bird_gymnasium
+import gymnasium
+env = gymnasium.make("FlappyBird-v0", render_mode="human", use_lidar=True)
 
-obs = env.reset()
+obs, _ = env.reset()
 while True:
     # Next action:
     # (feed the observation to your agent here)
-    action = ...  # env.action_space.sample() for a random action
+    action = env.action_space.sample()
 
     # Processing:
-    obs, reward, done, info = env.step(action)
+    obs, reward, terminated, _, info = env.step(action)
     
-    # Rendering the game:
-    # (remove this two lines during training)
-    env.render()
-    time.sleep(1 / 30)  # FPS
+    print(obs)
     
     # Checking if the player is still alive
-    if done:
+    if terminated:
         break
 
 env.close()
