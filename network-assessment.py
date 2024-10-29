dbbadfd9a403d0ae8f3f23801271aa3ad961cbe7
drewskotarczaknu <skotarczak.d@northeastern.edu>
2024-10-28 22:19:28 -0400

    added network-assessment which will load a .pkl and have it play 100 times then return the average score and average fitness

---
+++ network-assessment.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pickle
+import gymnasium # type: ignore
+
+def assess_trained_networks(pickle_filename, runs_per_network=10):
+    with open(pickle_filename, "rb") as f:
+        loaded_networks = pickle.load(f)
+    
+    env = gymnasium.make("FlappyBird-v0", render_mode=None, use_lidar=False)
+    
+    total_scores = []
+    total_fitness = []
+
+    for network in loaded_networks:
+        scores = []
+        fitness_values = []
+        
+        for _ in range(runs_per_network):
+            obs, _ = env.reset()
+            score = 0
+            total_reward = 0
+            
+            while True:
+                action_prob = network.forward(obs)
+                action = 1 if action_prob > 0 else 0
+                
+                obs, reward, terminated, _, info = env.step(action)
+                total_reward += reward
+                
+                # when pipe passed reward = 1
+                if reward == 1:
+                    score += 1
+                    
+                if terminated:
+                    break
+            
+            scores.append(score)
+            fitness_values.append(total_reward)
+        
+        avg_score = np.mean(scores)
+        avg_fitness = np.mean(fitness_values)
+        
+        total_scores.append(avg_score)
+        total_fitness.append(avg_fitness)
+    
+    overall_avg_score = np.mean(total_scores)
+    overall_avg_fitness = np.mean(total_fitness)
+    
+    env.close()
+    
+    print(f"Overall average score: {overall_avg_score}")
+    print(f"Overall average fitness: {overall_avg_fitness}")
+    return overall_avg_score, overall_avg_fitness
+
+
+assess_trained_networks(".pkl", runs_per_network=100)
