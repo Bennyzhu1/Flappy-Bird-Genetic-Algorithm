dc90a1bec1344e972fda6d4a4e68afffa26c655d
Amber <amberhorace@gmail.com>
2024-12-02 19:42:59 -0500

    Clean up code and calculate average scores

--- deepQLearning.py
+++ deepQLearning.py
@@ -18,10 +18,9 @@ epsilon_decay = 0.9995
 epsilon_min = 0.0001
 batch_size = 64
 memory_size = 50000
-target_update_frequency = 10  # Update target network every 100 episodes
+target_update_frequency = 10  
 num_episodes = 10000
 
-# Define the neural network model
 class DQN(nn.Module):
     def __init__(self, input_dim, output_dim):
         super(DQN, self).__init__()
@@ -35,18 +34,16 @@ class DQN(nn.Module):
         return self.output_layer(x)
 
 
-# Initialize Q-network and target network
 state_size = env.observation_space.shape[0]
 action_size = env.action_space.n
-policy_net = DQN(state_size, action_size) # Main neural network the agent uses to make decisions
-target_net = DQN(state_size, action_size) # Stabilizing mechanism that maintains a slower changing version of Q values used for training
+policy_net = DQN(state_size, action_size) 
+target_net = DQN(state_size, action_size) 
 target_net.load_state_dict(policy_net.state_dict())
 target_net.eval()
 
 optimizer = optim.Adam(policy_net.parameters(), lr=learning_rate)
 memory = deque(maxlen=memory_size)
 
-# Function to choose an action based on epsilon-greedy policy
 def choose_action(state, epsilon):
     if random.random() < epsilon:
         return random.choice([0, 1])  # Random action (exploration)
@@ -56,7 +53,6 @@ def choose_action(state, epsilon):
             q_values = policy_net(state)
             return q_values.argmax().item()  # Best action (exploitation)
 
-# Function to train the Q-network
 def replay():
     if len(memory) < batch_size:
         return
@@ -87,7 +83,6 @@ def replay():
     optimizer.step()
 
 def train(epsilon):
-    # Training loop
     for episode in range(num_episodes):
         state, _ = env.reset()
         done = False
@@ -95,22 +90,17 @@ def train(epsilon):
         score = 0
         
         while not done:
-            # Epsilon-greedy action selection
             action = choose_action(state, epsilon)
             
-            # Take action, observe reward and next state
             next_state, reward, done, _, _ = env.step(action)
             total_reward += reward
             if reward == 1:
                 score += 1
             
-            # Store experience in memory
             memory.append((state, action, reward, next_state, done))
             
-            # Update state
             state = next_state
             
-            # Train Q-network
             replay()
 
         # Save the trained model
@@ -130,38 +120,38 @@ def train(epsilon):
 
 
 
-# Play the game with the trained model
 def play_trained_model():
-    env = gymnasium.make('FlappyBird-v0', render_mode='human', use_lidar=False)
+    env = gymnasium.make('FlappyBird-v0', render_mode=None, use_lidar=False)
 
-     # Initialize a new model with the same architecture
     trained_model = DQN(state_size, action_size)
 
-    # Load the saved weights
     trained_model.load_state_dict(torch.load("flappybird_dqn_model_100_1.pth"))
-    trained_model.eval()  # Set the model to evaluation mode
+    trained_model.eval()  
 
-    state, _ = env.reset()
-    done = False
-    total_reward = 0
-    score = 0
+    total_score = 0
+    for episode in range(100):
+        state, _ = env.reset()
+        done = False
+        total_reward = 0
+        score = 0
 
-    while not done:
-        state_tensor = torch.FloatTensor(state).unsqueeze(0)
-        with torch.no_grad():
-            action = trained_model(state_tensor).argmax().item()  # Choose the best action
-        
-        # Step the environment with the chosen action
-        next_state, reward, done, _, _ = env.step(action)
-        total_reward += reward
+        while not done:
+            state_tensor = torch.FloatTensor(state).unsqueeze(0)
+            with torch.no_grad():
+                action = trained_model(state_tensor).argmax().item()  
+            
+            next_state, reward, done, _, _ = env.step(action)
+            total_reward += reward
 
-        if reward == 1:
-            score += 1
-        
-        # Update the state
-        state = next_state
+            if reward == 1:
+                score += 1
+            
+            state = next_state
 
-    print("Total Reward:", total_reward, "Score:", score)
+        print("Episode:", episode, "Total Reward:", total_reward, "Score:", score)
+        total_score += score
+
+    print("Finished playing, average score: ", total_score/100)
     env.close()
 
 train(epsilon)
